d84b614aed4c284d43299587a93afb419fdad514
abdul <abdul@DESKTOP-LLJAPN6>
2022-10-01 13:20:00 +0300

    dataTypes

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,20 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+		double sayi2 = 8.5;
+		char karakter = 'A' ;
+		System.out.println(sayi);
+		System.out.println(sayi2);
+		System.out.println(karakter);
+		
+		// byte, short, int, long tam sayı ifadeleridir
+		// float ve double ondalık sayı ifadeleridir.
+		// boolean şartlı bloklarda kullanılır (true or false) (1veya0)
+		// char tek karakter, tek tırnak kullanmak ZORUNLU
+
+	}
+
+}
