bc51cf648365d23f60b2b8df98720bee81e1bba6
abdul <abdul@DESKTOP-LLJAPN6>
2022-10-01 13:23:29 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci Sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : " + ogrenciSayisi);
+
+		System.out.println("Kursumuzdaki Toplam Öğrenci Sayısı : " + ogrenciSayisi);
+
+		int kurstakiOgrenciSayisi = 25645;
+		String mesaj2 = "Almanca Kursundaki Öğrenci Sayisi : ";
+		System.out.println(mesaj2 + kurstakiOgrenciSayisi);
+		System.out.println("Almanca Kursundaki Toplam Öğrenci Sayısı : " + kurstakiOgrenciSayisi);
+
+	}
+
+}
